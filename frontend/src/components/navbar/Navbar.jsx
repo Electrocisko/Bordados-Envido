303f9b5e85973d3769d9ba24331070f7b8b40a9e
Francisco Zuidwijk <electrocisko@gmail.com>
2024-01-30 15:12:01 -0300

    se agrego onclick navbar menu

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,30 +1,48 @@
+import { useState } from "react";
 import Closed from "../icons/Closed";
 import Hamburguer from "../icons/Hamburguer";
 import Logo from "../icons/Logo";
 import styles from "./navbar.module.scss";
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom";
 
 function Navbar() {
+
+const [showLinks, setShowLinks] = useState(false);
+
   return (
     <nav className={styles.navbar_container}>
-      <div className={styles.icon_hamburguer}> <Hamburguer color="white" width="25" height="18" /></div>
-     
+      <div className={styles.icon_hamburguer} onClick={() => setShowLinks(!showLinks)}  >
+        <Hamburguer color="white" width="25" height="18"  />
+      </div>
+
       <div className={styles.logo_container}>
         <p>BORDADOS ENVIDO</p>
         <Logo height={"40px"} />
       </div>
-      <p></p>
-      <div className={styles.links_container}>
-      <ul >
-        <li><Closed  color="white" width="26" height="26" /></li>
+      <p >Cart</p>
+        <div className={showLinks ? `${styles.links_container}`: `${styles.links_container} ${styles.display_hide}`}>
+        <ul>
+          <li onClick={() => setShowLinks(!showLinks)}>
+            <Closed color="white" width="26" height="26" />
+          </li>
 
-        <li><Link to="/">Inicio</Link></li>
-        <li> <Link to="/productos">Productos</Link></li>
-        <li> <Link to="/nosotros">Nosotros</Link></li>
-        <li> <Link to="/contacto">Contacto</Link></li>
-      </ul>
+          <li>
+            <Link to="/">Inicio</Link>
+          </li>
+          <li>
+            {" "}
+            <Link to="/productos">Productos</Link>
+          </li>
+          <li>
+            {" "}
+            <Link to="/nosotros">Nosotros</Link>
+          </li>
+          <li>
+            {" "}
+            <Link to="/contacto">Contacto</Link>
+          </li>
+        </ul>
       </div>
-
     </nav>
   );
 }

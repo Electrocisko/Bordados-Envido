5cb49128ebe862c1947b6d23b762ea9b864011c6
Francisco Zuidwijk <electrocisko@gmail.com>
2024-01-31 13:55:24 -0300

    agregue mixin con breakpoint

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -2,27 +2,21 @@ import { useState } from "react";
 import Closed from "../icons/Closed";
 import Hamburguer from "../icons/Hamburguer";
 import Logo from "../icons/Logo";
-import "./navbar.css";
+//import "./navbar.css";
+import styles from './navbar.module.scss'
 import { Link } from "react-router-dom";
 
 function Navbar() {
   const [showLinks, setShowLinks] = useState(false);
 
   return (
-    <nav className="navbar_container">
+    <nav className={styles.navbar_container}>
 
- 
-
-      <div className="logo_container">
+      <div className={styles.logo_container}>
         <p>BORDADOS ENVIDO</p>
         <Logo height={"40px"} />
       </div>
-
-      <div
-        className={
-          showLinks ? "links_container" : "links_container display_hide"
-        }
-      >
+       <div className={showLinks ? `${styles.links_container}`: `${styles.links_container} ${styles.display_hide}`}>
         <ul className="ul-list">
           <li onClick={() => setShowLinks(!showLinks)} className="close-icon">
             <Closed color="white" width="26" height="26" />
@@ -46,7 +40,7 @@ function Navbar() {
         </ul>
       </div>
 
-      <div className="icon_hamburguer" onClick={() => setShowLinks(!showLinks)}>
+      <div className={styles.icon_hamburguer} onClick={() => setShowLinks(!showLinks)}>
         <Hamburguer color="white" width="25" height="18" />
       </div>
     </nav>

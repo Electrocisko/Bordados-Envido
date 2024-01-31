3a9feb6cd8865d5d8ac31eec94bf1ba19c12167f
Francisco Zuidwijk <electrocisko@gmail.com>
2024-01-31 10:14:17 -0300

    navbar desktop design

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -6,27 +6,29 @@ import "./navbar.css";
 import { Link } from "react-router-dom";
 
 function Navbar() {
-
-const [showLinks, setShowLinks] = useState(false);
+  const [showLinks, setShowLinks] = useState(false);
 
   return (
     <nav className="navbar_container">
-      <div className="icon_hamburguer" onClick={() => setShowLinks(!showLinks)}  >
-        <Hamburguer color="white" width="25" height="18"  />
-      </div>
+
+ 
 
       <div className="logo_container">
         <p>BORDADOS ENVIDO</p>
         <Logo height={"40px"} />
       </div>
 
-          <div className={showLinks ? "links_container" : "links_container display_hide" }>
+      <div
+        className={
+          showLinks ? "links_container" : "links_container display_hide"
+        }
+      >
         <ul className="ul-list">
           <li onClick={() => setShowLinks(!showLinks)} className="close-icon">
             <Closed color="white" width="26" height="26" />
           </li>
 
-          <li onClick={() => setShowLinks(!showLinks)} >
+          <li onClick={() => setShowLinks(!showLinks)}>
             <Link to="/">Inicio</Link>
           </li>
           <li onClick={() => setShowLinks(!showLinks)}>
@@ -42,6 +44,10 @@ const [showLinks, setShowLinks] = useState(false);
             <Link to="/contacto">Contacto</Link>
           </li>
         </ul>
+      </div>
+
+      <div className="icon_hamburguer" onClick={() => setShowLinks(!showLinks)}>
+        <Hamburguer color="white" width="25" height="18" />
       </div>
     </nav>
   );

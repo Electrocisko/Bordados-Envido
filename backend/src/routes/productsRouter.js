94b09a00954445e139220ce56593c6535d648588
Francisco Zuidwijk <electrocisko@gmail.com>
2024-02-15 09:15:03 -0300

    trabajando en fetch

--- backend/src/routes/productsRouter.js
+++ backend/src/routes/productsRouter.js
@@ -1,29 +1,27 @@
 import express from "express";
 import upLoader from "../helpers/imageLoader.js";
-import {Product} from "../models/productModel.js";
+import { Product } from "../models/productModel.js";
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
+router.get("/productos", async (req, res) => {
+
+  const data = await Product.find();
   res.json({
     status: "success",
-    message: "Aca se muestran todos los productos",
+    data
   });
 });
 
 router.post("/ingresar", upLoader.single("file"), async (req, res) => {
   let newProduct = req.body;
-    newProduct.image = req.file.filename;
-    // modelo, categoria, descripcion, precio, file;
-
-    let product = new Product(newProduct);
-    let data = await product.save()
-    console.log(data);
-
+  newProduct.image = req.file.filename;
+  // modelo, categoria, descripcion, precio, file;
+  let product = new Product(newProduct);
+  let data = await product.save();
   res.json({
     status: "succes",
-    message: "Aca se ingresa los datos a Api",
-   data
+    data,
   });
 });
 

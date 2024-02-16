102f369e2e9abd58e2ea6bde2b18bbf8611bd1ea
Francisco Zuidwijk <electrocisko@gmail.com>
2024-02-16 08:28:15 -0300

    validaciones en formulario

--- backend/src/controllers/productsControllers.js
+++ backend/src/controllers/productsControllers.js
@@ -1,66 +1,72 @@
-import {Product} from '../models/productModel.js';
-import fs from 'fs';
+import { Product } from "../models/productModel.js";
+import fs from "fs";
 
- export const getProducts = async (req,res) => {
-    try {
-        const  data = await Product.find().lean();
-        res.status(200).json({
-            status: "success",
-            data
-          });
+export const getProducts = async (req, res) => {
+  try {
+    const data = await Product.find().lean();
+    res.status(200).json({
+      status: "success",
+      data,
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: "error",
+      message: "Error en obtener productos",
+    });
+  }
+};
 
-    } catch (error) {
-        res.status(500).json({
-            status: "error",
-            message: "Error en obtener productos"
-        })
+export const createProduct = async (req, res) => {
+  try {
+    let newProduct = req.body;
+    const {modelo, categoria, precio } = newProduct;
+    if (req.file) {
+      newProduct.image = req.file.filename;
+      const imageSplit = req.file.filename.split(".");
+      const extension = imageSplit[1];
+      if (extension != "png" && extension != "jpg" && extension != "webp") {
+        throw new Error("Archivo adjunto no valido.");
+      }
+      if (req.file.size > 300000)
+        throw new Error("El archivo no puede superar 300Kb");
+    } else {
+      newProduct.image = "logo-envido.png";
     }
-}
-
-export const createProduct = async (req,res) => {
-    try {
-        let newProduct = req.body;
-        newProduct.image = req.file.filename;
-        //Faltan validaciones!! de los datos que llegan
-        const imageSplit = req.file.filename.split(".");
-        const extension = imageSplit[1];
-        if (extension != "png" && extension != "jpg" && extension != "webp") {
-            throw new Error("Archivo adjunto no valido.");
-        }
-
-        if(req.file.size > 300000) throw new Error("Ups muy pesado");
-        
-        let product = new Product(newProduct);
-        let data = await product.save();
-        res.status(200).json({
-          status: "succes",
-          data,
-        });
-    } catch (error) {
-        const filePath = req.file.path;
-        fs.unlinkSync(filePath);
-        res.status(500).json({
-            status: "error",
-            message: "Error en crear producto",
-            error: error.message
-        })
+    if (!modelo || categoria == "Seleccione categoría" || !precio) {
+        throw new Error("Falta datos")
     }
-}
-
-export const getProductsCat = async (req,res) => {
-    try {
-        console.log(req.params);
-        let param = req.params;
-        const data = await Product.find({categoria: param.cat }).lean();
-        res.status(200).json({
-            status:"succes",
-            data
-        })
-    } catch (error) {
-        res.status(500).json({
-            status: "error",
-            message: "Error en mostrar productos por categoria",
-            error: error.message
-        })
+    let product = new Product(newProduct);
+    let data = await product.save();
+    res.status(200).json({
+      status: "succes",
+      data,
+    });
+  } catch (error) {
+    if (req.file) {
+      const filePath = req.file.path;
+      fs.unlinkSync(filePath);
     }
-}
+
+    res.status(500).json({
+      status: "error",
+      message: error.message,
+    });
+  }
+};
+
+export const getProductsCat = async (req, res) => {
+  try {
+    let param = req.params;
+    const data = await Product.find({ categoria: param.cat }).lean();
+    res.status(200).json({
+      status: "succes",
+      data,
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: "error",
+      message: "Error en mostrar productos por categoria",
+      error: error.message,
+    });
+  }
+};
--- backend/src/public/scripts/form.js
+++ backend/src/public/scripts/form.js
@@ -1,23 +1,24 @@
-
 const form = document.getElementById("formulario");
+const route = "http://localhost:8080/api/ingresar";
 
-
-const route = "http://localhost:8080/api/ingresar "
-
-  form.addEventListener("submit", (e) => {
+form.addEventListener("submit", (e) => {
   e.preventDefault();
+
   let data = new FormData(form);
 
   fetch(route, {
     method: "POST",
     body: data,
-  });
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.status === "succes") {
+        Swal.fire("Agregado");
+        form.reset();
+      } else {
+        Swal.fire(data.message);
+      }
+    });
+});
 
-// Falta un sweetAlert!!
 
-  form.reset();
-
-
-
- 
-  });

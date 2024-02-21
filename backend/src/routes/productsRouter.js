85f76a1b0e5f2f775c9d25f414e4996180a2c27d
Francisco Zuidwijk <electrocisko@gmail.com>
2024-02-21 17:05:21 -0300

    se agrego autenticacion por token en api

--- backend/src/routes/productsRouter.js
+++ backend/src/routes/productsRouter.js
@@ -1,15 +1,16 @@
 import express from "express";
 import upLoader from "../helpers/imageLoader.js";
 import {getProducts, createProduct, getProductsCat, deleteProductById, modifiedProductById} from "../controllers/productsControllers.js";
+import auth from "../middlewares/auth.js";
 
 const router = express.Router();
 
 router.get("/productos", getProducts); // Publico
 router.get("/productos/:cat", getProductsCat); //Publico
 
-router.post("/ingresar", upLoader.single("file"), createProduct); // Privado
-router.delete("/eliminar/:id",deleteProductById); //Privado
-router.patch("/modificar/:id", upLoader.single("file"), modifiedProductById); //Privado
+router.post("/ingresar", auth , upLoader.single("file"), createProduct); // Privado
+router.delete("/eliminar/:id", auth, deleteProductById); //Privado
+router.patch("/modificar/:id", auth, upLoader.single("file"), modifiedProductById); //Privado
 
 
 

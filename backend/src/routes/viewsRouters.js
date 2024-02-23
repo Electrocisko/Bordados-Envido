87cf3195e665736c0c294750ea12f29a013756b5
Francisco Zuidwijk <electrocisko@gmail.com>
2024-02-23 13:48:48 -0300

    Se agrego validaciones en todos los endpoints privados

--- backend/src/routes/viewsRouters.js
+++ backend/src/routes/viewsRouters.js
@@ -2,6 +2,7 @@ import express from "express";
 import { Product } from "../models/productModel.js";
 import checkCookie from "../middlewares/checkToken.js";
 
+
 const router = express.Router();
 
 router.get("/", (req, res) => {
@@ -14,17 +15,33 @@ router.get("/login", (req, res) => {
 
 router.get("/form", checkCookie, (req, res) => {
   let token = req.cookies.jwt;
-  res.render("form.ejs", {token: token});
+  res.render("form.ejs", { token: token });
 });
 
+router.get("/modificar/:id", checkCookie, async (req, res) => {
+  try {
+    let id = req.params.id;
+    let product = await Product.findById(id).lean();
+    let token = req.cookies.jwt; 
+    res.render("updatedForm.ejs", { id: id, product: product, token: token });
+  } catch (error) {
+    res.status(400).json({
+      status:"error",
+      message: error.message
+    })
+  }
+});
 
 router.get("/admin", checkCookie, async (req, res) => {
   try {
     let token = req.cookies.jwt;
     const data = await Product.find().lean();
-    res.render("products.ejs", { data: data, token: token});
+    res.render("products.ejs", { data: data, token: token });
   } catch (error) {
-    console.log(error);
+    res.status(400).json({
+      status:"error",
+      message: error.message
+    })
   }
 });
 

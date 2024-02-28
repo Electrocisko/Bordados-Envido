96cd339fcb315b2c362d29864ee78f201e7bafeb
Francisco Zuidwijk <electrocisko@gmail.com>
2024-02-28 08:50:14 -0300

    se cambio icono de instagram

--- frontend/src/components/icons/Instagram.jsx
+++ frontend/src/components/icons/Instagram.jsx
@@ -1,7 +1,7 @@
 
 function Instagram() {
   return (
-    <img src="./images\2227.jpg" alt="Icono De Instagram"   height={"40px"}/>
+    <img src="./images/instagram.png" alt="Icono De Instagram"   height={"40px"}/>
   )
 }
 

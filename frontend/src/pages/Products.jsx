f31517901f54c5eee39e1dc61a48f1d986d73435
Francisco Zuidwijk <electrocisko@gmail.com>
2024-03-07 11:37:58 -0300

    trabajando en sort

--- frontend/src/pages/Products.jsx
+++ frontend/src/pages/Products.jsx
@@ -10,6 +10,43 @@ function Products() {
   const {products} = useLoaderData();
   const {data} = products
 
+
+const sortByModel = (dataToSort) => {
+  dataToSort.sort((a,b)=> {
+    if (a.modelo > b.modelo) {
+      return 1;
+    }
+    if (a.modelo < b.modelo) {
+      return -1;
+    }
+    return 0;
+  });
+}
+
+const sortByOldest = (dataToSort) => {
+  dataToSort.sort((a,b)=> {
+    if (a.iat > b.iat) {
+      return 1;
+    }
+    if (a.iat < b.iat) {
+      return -1;
+    }
+    return 0;
+  });
+}
+
+
+//console.log(sortByModel(data));
+
+sortByOldest(data)
+
+
+
+
+
+
+
+
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Productos</h1>
